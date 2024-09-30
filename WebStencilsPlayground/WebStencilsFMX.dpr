108cd4949d462e52fa4226fd5bfe96122a241111
Ian Barker <ian.barker@gmail.com>
2024-09-30 16:34:13 -0500

    Initial commit

---
+++ WebStencilsPlayground/WebStencilsFMX.dpr
@@ -0,0 +1,16 @@
+﻿program WebStencilsFMX;
+
+uses
+  System.StartUpCopy,
+  FMX.Forms,
+  MainForm in 'MainForm.pas' {WebStencilsPlayground},
+  PersonClass in 'PersonClass.pas',
+  MyTemplates in 'MyTemplates.pas';
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.CreateForm(TWebStencilsPlayground, WebStencilsPlayground);
+  Application.Run;
+end.
