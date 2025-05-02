1c6caf1a1956f5c68e95b83a25302936934dc658
azapater <1220658+azapater@users.noreply.github.com>
2025-05-02 15:39:50 +0200

    indentation fix WB Delphi

--- WebBrokerProject/Delphi/MainWebModuleU.pas
+++ WebBrokerProject/Delphi/MainWebModuleU.pas
@@ -3,100 +3,100 @@
 interface
 
 uses
-	 // System units
-	System.Classes,
-	System.Generics.Collections,
-	System.IOUtils,
-	System.SysUtils,
+   // System units
+  System.Classes,
+  System.Generics.Collections,
+  System.IOUtils,
+  System.SysUtils,
 
-	// Data units
-	Data.DB,
+  // Data units
+  Data.DB,
 
-	// Web units
-	Web.HTTPApp,
-	Web.Stencils,
+  // Web units
+  Web.HTTPApp,
+  Web.Stencils,
 
-	// FireDAC
-	FireDAC.Stan.Async,
-	FireDAC.Stan.Def,
-	FireDAC.Stan.Error,
-	FireDAC.Stan.ExprFuncs,
-	FireDAC.Stan.Intf,
-	FireDAC.Stan.Option,
-	FireDAC.Stan.Param,
-	FireDAC.Stan.Pool,
-	FireDAC.Stan.StorageJSON,
-	FireDAC.DApt,
-	FireDAC.DApt.Intf,
-	FireDAC.DatS,
-	FireDAC.Phys,
-	FireDAC.Phys.Intf,
-	FireDAC.Phys.SQLite,
-	FireDAC.Phys.SQLiteDef,
-	FireDAC.Phys.SQLiteWrapper.Stat,
-	FireDAC.ConsoleUI.Wait,
-	FireDAC.Comp.Client,
-	FireDAC.Comp.DataSet,
-	FireDAC.UI.Intf,
+  // FireDAC
+  FireDAC.Stan.Async,
+  FireDAC.Stan.Def,
+  FireDAC.Stan.Error,
+  FireDAC.Stan.ExprFuncs,
+  FireDAC.Stan.Intf,
+  FireDAC.Stan.Option,
+  FireDAC.Stan.Param,
+  FireDAC.Stan.Pool,
+  FireDAC.Stan.StorageJSON,
+  FireDAC.DApt,
+  FireDAC.DApt.Intf,
+  FireDAC.DatS,
+  FireDAC.Phys,
+  FireDAC.Phys.Intf,
+  FireDAC.Phys.SQLite,
+  FireDAC.Phys.SQLiteDef,
+  FireDAC.Phys.SQLiteWrapper.Stat,
+  FireDAC.ConsoleUI.Wait,
+  FireDAC.Comp.Client,
+  FireDAC.Comp.DataSet,
+  FireDAC.UI.Intf,
 
-	// Own units
-	Helpers.WebModule,
-	Helpers.FDQuery,
-	Controller.Tasks,
-	Model.Tasks,
-	Controller.Customers,
+  // Own units
+  Helpers.WebModule,
+  Helpers.FDQuery,
+  Controller.Tasks,
+  Model.Tasks,
+  Controller.Customers,
     CodeExamplesU;
 
 type
-	{ TEnvironmentSettings: Class to hold environment/application settings for WebStencils }
-	TEnvironmentSettings = class(TPersistent)
-	private
-		FAppVersion: string;
-		FAppName: string;
-		FAppEdition: string;
-		FCompanyName: string;
-		FResource: string;
-		FDebugMode: Boolean;
+  { TEnvironmentSettings: Class to hold environment/application settings for WebStencils }
+  TEnvironmentSettings = class(TPersistent)
+  private
+    FAppVersion: string;
+    FAppName: string;
+    FAppEdition: string;
+    FCompanyName: string;
+    FResource: string;
+    FDebugMode: Boolean;
     FIsRadServer: Boolean;
-	public
-		constructor Create;		
-	published
-		property AppVersion: string read FAppVersion;
-		property AppName: string read FAppName;
-		property AppEdition: string read FAppEdition;
-		property CompanyName: string read FCompanyName;
-		property Resource: string read FResource; // Required for RAD Server compatibility
-		property DebugMode: Boolean read FDebugMode;
-		property IsRadServer: Boolean read FIsRadServer;
-	end;
+  public
+    constructor Create;    
+  published
+    property AppVersion: string read FAppVersion;
+    property AppName: string read FAppName;
+    property AppEdition: string read FAppEdition;
+    property CompanyName: string read FCompanyName;
+    property Resource: string read FResource; // Required for RAD Server compatibility
+    property DebugMode: Boolean read FDebugMode;
+    property IsRadServer: Boolean read FIsRadServer;
+  end;
 
-	TMainWebModule = class(TWebModule)
-		WebStencilsEngine: TWebStencilsEngine;
-		// Adding to WebStencils an object/component using attributes
-		WebFileDispatcher: TWebFileDispatcher;
-		[WebStencilsVar('customers', false)]
-		Customers: TFDQuery;
-		Connection: TFDConnection;
-		constructor Create(AOwner: TComponent); override;
-		destructor Destroy; override;
-		procedure WebStencilsEngineValue(Sender: TObject;
-			const AObjectName, APropName: string; var AReplaceText: string;
-			var AHandled: Boolean);
-	private
-		FTasksController: TTasksController;
-		FCustomersController: TCustomersController;
-		FCodeExamples: TCodeExamples;
-		FEnvironmentSettings: TEnvironmentSettings;
-		FResourcesPath: string;
-		procedure DefineRoutes;
-		procedure InitRequiredData;
-		procedure InitControllers;
-	public
-		{ Public declarations }
-	end;
+  TMainWebModule = class(TWebModule)
+    WebStencilsEngine: TWebStencilsEngine;
+    // Adding to WebStencils an object/component using attributes
+    WebFileDispatcher: TWebFileDispatcher;
+    [WebStencilsVar('customers', false)]
+    Customers: TFDQuery;
+    Connection: TFDConnection;
+    constructor Create(AOwner: TComponent); override;
+    destructor Destroy; override;
+    procedure WebStencilsEngineValue(Sender: TObject;
+      const AObjectName, APropName: string; var AReplaceText: string;
+      var AHandled: Boolean);
+  private
+    FTasksController: TTasksController;
+    FCustomersController: TCustomersController;
+    FCodeExamples: TCodeExamples;
+    FEnvironmentSettings: TEnvironmentSettings;
+    FResourcesPath: string;
+    procedure DefineRoutes;
+    procedure InitRequiredData;
+    procedure InitControllers;
+  public
+    { Public declarations }
+  end;
 
 var
-	WebModuleClass: TComponentClass = TMainWebModule;
+  WebModuleClass: TComponentClass = TMainWebModule;
 
 implementation
 
@@ -107,18 +107,18 @@ implementation
 
 constructor TEnvironmentSettings.Create;
 begin
-	inherited Create;
-	// Initialize properties
-	FAppVersion := '1.0.0';
-	FAppName := 'WebStencils demo';
-	FAppEdition := 'WebBroker Delphi';
-	FCompanyName := 'Embarcadero Inc.';
-	// This RESOURCE env is required to make the WebStencils templates reusable for RAD Server
-	FResource := '';
+  inherited Create;
+  // Initialize properties
+  FAppVersion := '1.0.0';
+  FAppName := 'WebStencils demo';
+  FAppEdition := 'WebBroker Delphi';
+  FCompanyName := 'Embarcadero Inc.';
+  // This RESOURCE env is required to make the WebStencils templates reusable for RAD Server
+  FResource := '';
 {$IFDEF DEBUG}
-	FDebugMode := True;
+  FDebugMode := True;
 {$ELSE}
-	FDebugMode := False;
+  FDebugMode := False;
 {$ENDIF}
   FIsRadServer := False;
 end;
@@ -127,43 +127,43 @@ end;
 
 constructor TMainWebModule.Create(AOwner: TComponent);
 begin
-	inherited;
-	InitControllers;
-	InitRequiredData;
-	DefineRoutes;
+  inherited;
+  InitControllers;
+  InitRequiredData;
+  DefineRoutes;
 end;
 
 destructor TMainWebModule.Destroy;
 begin
-	Customers.Active := false;
-	FTasksController.Free;
-	FCustomersController.Free;
+  Customers.Active := false;
+  FTasksController.Free;
+  FCustomersController.Free;
   FCodeExamples.Free;
   FEnvironmentSettings.Free; // Free the settings object
-	inherited;
+  inherited;
 end;
 
 procedure TMainWebModule.InitControllers;
 begin
-	FTasksController := TTasksController.Create(WebStencilsEngine);
-	FCustomersController := TCustomersController.Create(WebStencilsEngine, Customers);
-	// More Controllers can be initialized here
+  FTasksController := TTasksController.Create(WebStencilsEngine);
+  FCustomersController := TCustomersController.Create(WebStencilsEngine, Customers);
+  // More Controllers can be initialized here
 end;
 
 procedure TMainWebModule.InitRequiredData;
 begin
-	// Set the path for resources based on the platform
-	// Check Project/Deployment. The required resources are added there for Linux Deployment using PAServer
-	// For simplifying the demo, on Windows the path of the templates is on the same project folder. Release deployment would need a fix in the Path
-	var BinaryPath := TPath.GetDirectoryName(ParamStr(0));
+  // Set the path for resources based on the platform
+  // Check Project/Deployment. The required resources are added there for Linux Deployment using PAServer
+  // For simplifying the demo, on Windows the path of the templates is on the same project folder. Release deployment would need a fix in the Path
+  var BinaryPath := TPath.GetDirectoryName(ParamStr(0));
 {$IFDEF MSWINDOWS}
-	FResourcesPath := TPath.Combine(BinaryPath, '../../../../resources');
+  FResourcesPath := TPath.Combine(BinaryPath, '../../../../resources');
 {$ELSE}
-	FResourcesPath := BinaryPath;
+  FResourcesPath := BinaryPath;
 {$ENDIF}
-	WebStencilsEngine.RootDirectory := TPath.Combine(FResourcesPath, 'html');
-	WebFileDispatcher.RootDirectory := WebStencilsEngine.RootDirectory;
-	Connection.Params.Database := TPath.Combine(FResourcesPath, 'data/database.sqlite3');
+  WebStencilsEngine.RootDirectory := TPath.Combine(FResourcesPath, 'html');
+  WebFileDispatcher.RootDirectory := WebStencilsEngine.RootDirectory;
+  Connection.Params.Database := TPath.Combine(FResourcesPath, 'data/database.sqlite3');
   FCodeExamples := TCodeExamples.Create(WebStencilsEngine);
 
   // Create the environment settings object
@@ -185,23 +185,23 @@ begin
       AReplaceText := FormatDateTime('yyyy', Now)
     else
       AReplaceText := Format('SYSTEM_%s_NOT_FOUND', [APropName.ToUpper]);
-	AHandled := True;      
+  AHandled := True;      
   end;
 end;
 
 procedure TMainWebModule.DefineRoutes;
 begin
-	// Define the application's routes using a declarative approach.
-	// This class helper maps HTTP methods and paths to their respective handler methods.
-	AddRoutes([TRoute.Create(mtDelete, '/tasks', FTasksController.DeleteTask),
-		TRoute.Create(mtPost, '/tasks/add', FTasksController.CreateTask),
-		TRoute.Create(mtGet, '/tasks/edit', FTasksController.GetEditTask),
-		TRoute.Create(mtPut, '/tasks/toggleCompleted', FTasksController.TogglecompletedTask),
+  // Define the application's routes using a declarative approach.
+  // This class helper maps HTTP methods and paths to their respective handler methods.
+  AddRoutes([TRoute.Create(mtDelete, '/tasks', FTasksController.DeleteTask),
+    TRoute.Create(mtPost, '/tasks/add', FTasksController.CreateTask),
+    TRoute.Create(mtGet, '/tasks/edit', FTasksController.GetEditTask),
+    TRoute.Create(mtPut, '/tasks/toggleCompleted', FTasksController.TogglecompletedTask),
     TRoute.Create(mtPut, '/tasks', FTasksController.EditTask),
-		// Customers routes
-		TRoute.Create(mtGet, '/bigtable', FCustomersController.GetAllCustomers),
-		TRoute.Create(mtGet, '/pagination', FCustomersController.GetCustomers)
-		]);
+    // Customers routes
+    TRoute.Create(mtGet, '/bigtable', FCustomersController.GetAllCustomers),
+    TRoute.Create(mtGet, '/pagination', FCustomersController.GetCustomers)
+    ]);
 end;
 
 end.
